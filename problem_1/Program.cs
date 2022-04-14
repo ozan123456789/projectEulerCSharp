58cd065e4ab06452233cae7fbee0cc2db20cb87c
Ozan Akkaşoğlu <ozan470@hotmail.com>
2022-04-14 18:21:19 +0300

    Solution_of_problem1 is added

---
+++ problem_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(multiples_of());
+        }
+
+        static int multiples_of(int n1=3,int n2=5,int limit=1000)
+        {
+            var sum = 0;
+
+            for(int i=3; i<limit; i++)
+            {
+                if(i %3 == 0 || i % 5 == 0){
+                    sum += i;
+            }
+            }
+
+            return sum;
+        }
+    }
+}
