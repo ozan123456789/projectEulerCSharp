81c006c24d22acd86511b48994d29a760bdb316b
Ozan Akkaşoğlu <ozan470@hotmail.com>
2022-04-16 16:33:52 +0300

    problem 1 is solved

---
+++ problem_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace problem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solution of problem 2:" + solution2());
+        }
+
+
+       static int solution2(int limit = 4000000)
+        {
+            int sum = 0;
+            int first = 1,
+                second = 2,
+                third = first + second;
+
+            while (third<limit)
+            {
+                if (third % 2 == 0)
+                {
+                    sum += third;
+                }
+
+                first = second;
+                second = third;
+                third = first + second;
+
+                
+
+            }
+
+
+            return third;
+        }
+    }
+}
